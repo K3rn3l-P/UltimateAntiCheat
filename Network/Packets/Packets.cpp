bc87f0f3aaa5264fdfcc9839bfb0fd3cefc0e0d4
K3rn3l-P <davidcocco@outlook.it>
2025-06-05 21:12:25 +0200

    MOD

--- Network/Packets/Packets.cpp
+++ Network/Packets/Packets.cpp
@@ -32,46 +32,46 @@ PacketWriter* Packets::Builder::ClientHello(
 
 PacketWriter* Packets::Builder::ClientGoodbye(int reason)
 {
-	PacketWriter* p = new PacketWriter(Packets::Opcodes::CS_GOODBYE);
-	p->Write<int>(reason);
-	return p;
+    PacketWriter* p = new PacketWriter(Packets::Opcodes::CS_GOODBYE);
+    p->Write<int>(reason);
+    return p;
 }
 
 PacketWriter* Packets::Builder::DetectedCheater(int flags) //todo: finish these
 {
-	PacketWriter* p = new PacketWriter(Packets::Opcodes::CS_FLAGGED_CHEATER);
-	p->Write<int>(flags);
-	return p;
+    PacketWriter* p = new PacketWriter(Packets::Opcodes::CS_FLAGGED_CHEATER);
+    p->Write<int>(flags);
+    return p;
 }
 
 /*
-	DetectedCheater - flag a user as cheating, with some string data about what it found
+    DetectedCheater - flag a user as cheating, with some string data about what it found
 */
 PacketWriter* Packets::Builder::DetectedCheater(__in const uint32_t flags, __in const  const std::string detectedModule, __in const DWORD pid)
 {
-	PacketWriter* p = new PacketWriter(Packets::Opcodes::CS_FLAGGED_CHEATER);
-	p->Write<uint32_t>(flags);
-	p->Write<uint32_t>(pid);
-	p->WriteString(detectedModule);
-	return p;
+    PacketWriter* p = new PacketWriter(Packets::Opcodes::CS_FLAGGED_CHEATER);
+    p->Write<uint32_t>(flags);
+    p->Write<uint32_t>(pid);
+    p->WriteString(detectedModule);
+    return p;
 }
 
 PacketWriter* Packets::Builder::Heartbeat(const char* cookie_str) //todo: add more into this packet, such as integrity checking or detected flags.
 {
-	PacketWriter* p = new PacketWriter(Packets::Opcodes::CS_HEARTBEAT);
-	p->WriteString(cookie_str);
-	return p;
+    PacketWriter* p = new PacketWriter(Packets::Opcodes::CS_HEARTBEAT);
+    p->WriteString(cookie_str);
+    return p;
 }
 
 PacketWriter* Packets::Builder::QueryMemory(byte* bytestring, int size)
 {
-	PacketWriter* p = new PacketWriter(Packets::Opcodes::CS_QUERY_MEMORY);
-	p->Write<uint16_t>(size);
+    PacketWriter* p = new PacketWriter(Packets::Opcodes::CS_QUERY_MEMORY);
+    p->Write<uint16_t>(size);
 
-	for (int i = 0; i < size; i++)
-	{
-		p->Write<BYTE>(bytestring[i]);
-	}
+    for (int i = 0; i < size; i++)
+    {
+        p->Write<BYTE>(bytestring[i]);
+    }
 
-	return p;
+    return p;
 }
